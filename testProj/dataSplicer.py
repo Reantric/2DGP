Info box fixed, scaling works for increasing values
Information box now contains information and is fixed to the right half side of the canvas. The scaling is no longer buggy (for upwards values) and the aesthetics look pleasing (to me!)

testProj/dataSplicer.py
@@ -15,7 +15,7 @@ from scipy.interpolate import make_interp_spline, BSpline
 # .
 # .
 
-numOfDataPoints = 3000
+numOfDataPoints = 3500
 
 
 # len(lst) - 2 will give you the amount of duplicates!