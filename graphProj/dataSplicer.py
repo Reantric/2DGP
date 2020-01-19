c6022486c73383545b9bae3aca8a841d0cbfef1c
Arnav Chaku <44213272+protonlaser91@users.noreply.github.com>
2020-01-18 22:55:32 -0500

    maintenance
    Deletion of unnecessary files and python generality with test.txt and datas.txt not being absolute paths but rather relative paths.

--- graphProj/dataSplicer.py
+++ graphProj/dataSplicer.py
@@ -1,5 +1,9 @@
 import numpy as np
 from scipy.interpolate import make_interp_spline, BSpline
+from time import sleep
+from timeit import default_timer as timer
+
+
 
 # john bandy shishir
 # 1 1 4 7
@@ -15,101 +19,132 @@ from scipy.interpolate import make_interp_spline, BSpline
 # .
 # .
 
-numOfDataPoints = 500
 
-#len(lst) - 2 will give you the amount of duplicates!
-def espace(dataLst):
-    #n param needed for non-splined version!
-    #fix edge case! #if n is 20 and we got 4 items, div to 6.666666
+# (400-200)/0.1
+
+spacing = 4
+
+# len(lst) - 2 will give you the amount of duplicates!
+def espace(data):
+    xSpl = [a[0] for a in data[key[0]]]
+    dataPointSum = sum(xSpl)
+    coords = {x[a]: [] for a in range(numOfDataPoints)}  # gets X values!
+    # n param needed for non-splined version!
+    # fix edge case! #if n is 20 and we got 4 items, div to 6.666666
 
     # nMod = numOfDataPoints % (len(dataLst)-1)
     # dataPointY = np.zeros(0)
     # for coords in range(len(dataLst)-1):
     #     if coords == len(dataLst) - 1 - nMod:
-     #        n += 1
+    #        n += 1
     #         print(f"This should be printed approximately {nMod} times!")
     #     y1,y2 = dataLst[coords][1],dataLst[coords+1][1]
-       # print(y1,y2)
-        #linspace [start,stop]
-      #   firstWaveY = np.linspace(y1,y2,n)
-      #   dataPointY = np.concatenate((dataPointY, firstWaveY))
+    # print(y1,y2)
+    # linspace [start,stop]
+    #   firstWaveY = np.linspace(y1,y2,n)
+    #   dataPointY = np.concatenate((dataPointY, firstWaveY))
 
-    #only separate if this is run more than once!, how to add 1 more to list??
-   #  for m in range(len(dataLst)-1):
-      #   if m == len(dataLst) - 2:
-        #     dataPointY = np.delete(dataPointY,n*m)
+    # only separate if this is run more than once!, how to add 1 more to list??
+    #  for m in range(len(dataLst)-1):
+    #   if m == len(dataLst) - 2:
+    #     dataPointY = np.delete(dataPointY,n*m)
     #     else:
-     #       np.delete(dataPointY, n * m)
+    #       np.delete(dataPointY, n * m)
 
+    # print(len(dataPointY))  # quick check: 20 if 3 entries!
+    for o in data:
+        ySpl = [b[1] for b in data[o]]
+        negCount = ySpl.count(-1)
+      #  sleep(5)
+        m = 3
+        spl = make_interp_spline(xSpl[negCount:], ySpl[negCount:], k=m)
 
-   # print(len(dataPointY))  # quick check: 20 if 3 entries!
+        limitAmt = int((xSpl[negCount] - xSpl[0])/spacing)
+        zeroList = [-1] * limitAmt
+        dataPointY = np.concatenate([zeroList,spl(x[limitAmt:])])
+        for u in range(numOfDataPoints):
+            coords[x[u]] += [round(dataPointY[u],5)]
 
-    xSpl = [a[0] for a in dataLst]
-    ySpl = [b[1] for b in dataLst]
-    k = 2
-    spl = make_interp_spline(xSpl,ySpl,k=k)
-    dataPointY = spl(x)
-    #print(f"Spline object: {spl}: Splined list: {spl(x)}")
-    return dataPointY
+    # print(f"Spline object: {spl}: Splined list: {spl(x)}")
+    return coords
     # dataPointY = [np.delete(dataPointY,10*m) for m in range(len(dataLst)-1)][-1] horribly inefficient fix!
 
 
-
-   ####PROGRAM OUTPUTS FUNCTIONALLY!
-with open("C:\\Users\\arnav\\Desktop\\graphProj\\test.txt", mode='r+') as f:
+####PROGRAM OUTPUTS FUNCTIONALLY!
+with open("test.txt", mode='r+') as f:
     f.seek(0)
     key = f.readline()
-    s = []; x = []; y = []
     data = f.readlines()
-    #print(key)
-   # print(file[0])
+    # print(key)
+    # print(file[0])
     key = key.split()
-    d = {key[a]:[] for a in range(len(key))} #python i love you!
-   # print(key)
+    d = {key[a]: [] for a in range(len(key))}  # python i love you!
+    # print(key)
     for v in data:
         v = v.split()
         x = int(v.pop(0))
-        v = list(map(int,v))
-       # print(v)
-        for i in range(len(key)): ###HUGE CHANGE, REMOVING TUPLE ELEMENT INSIDE LIST! finished
-            d[key[i]] += [(x,v[i])]
-    print(d)
+        v = list(map(int, v))
+        # print(v)
+        for i in range(len(key)):  ###HUGE CHANGE, REMOVING TUPLE ELEMENT INSIDE LIST! finished
+            d[key[i]] += [(x, v[i])]
+    # print(d)
     xMax = list(d.values())[0][-1][0]
-    xMin = list(d.values())[0][0][0]
+    xMin = list(d.values())[0][0][0]  # disgusting!
+
 f.close()
 
-#print(numOfDataPoints)
-#numOfDataPoints += (len(d[key[0]])-2)
-x = np.linspace(xMin,xMax,numOfDataPoints) #it does work! (if not, subtract the len thing)
+numOfDataPoints = int((xMax - xMin) / spacing)  # make this (max-min)/n where n is spacing
+# print(numOfDataPoints)
+# numOfDataPoints += (len(d[key[0]])-2)
+x = np.linspace(xMin, xMax, numOfDataPoints)  # it does work! (if not, subtract the len thing) nice! - 2020
 
-n = int((numOfDataPoints+(len(d[key[0]])-2))/(len(d[key[0]])-1)) #n defined here!
+n = int((numOfDataPoints + (len(d[key[0]]) - 2)) / (len(d[key[0]]) - 1))  # n defined here!
 
 print(f"Val of n: {n}")
-#print("AAAAAAAAAA")
+# print("AAAAAAAAAA")
 print(f"Length of x: {len(x)}")
 # {john: [(x1,y1),(x2,y2)], shishir: [(x1,y1),()]
 
-with open("C:\\Users\\arnav\\Desktop\\graphProj\\datas.txt", mode='w+') as f:
-    f.write(' '.join(key))
-    for k in key:
-        # print(d[k])
-        y = espace(d[k])
-        print(f"Length of y: {len(y)}")
-        d[k].clear()
-        d[k] += list(zip(x,y))
-    #print(d)
+with open("datas.txt", mode='w+') as f:
+    f.write(' '.join(key) + "\n")
+    #  for k in key:
+    # print(d[k])
+    #     y = espace(d[k])
+    #      print(f"Length of y: {len(y)}")
+    #      d[k].clear()
+    #      d[k] += list(zip(x, y))
+    # print(d)
+    start = timer()
+    print("Calculating")
+    coords = espace(d)
+    # print(y)
+    print("Beginning!")
+    # print(" ".join(map(str,coords[x[2]])))
+    statement = ""
+
     for ind in range(numOfDataPoints - 1):
-        f.write(f"\n{str(round(x[ind],3))} ")
-        for k in key:
-            writeY = list(d[k])[ind][1]
-            if k == key[-1]:
-                f.write(f"{str(round(writeY,3))}")
-            else:
-                f.write(f"{str(round(writeY, 3))} ")
-        #print(f"({x[e]},{3})")
+      #  if ind % 400 == 0: tester
+       #     print(f"{100 * ind / (numOfDataPoints - 1)}% done!")
+        # f.write(f"\n{str(round(x[ind], 3))} ")
+        statement += f"{str(round(x[ind], 5))} " + " ".join(map(str, coords[x[ind]])) + "\n"
+        # heres the genius! ^^
+    f.write(statement)
+    end = timer()
+
+    print(f"Finished in {(end-start)} sec")
+    #there is no way this will be faster! danggg
+
+    #   for k in key:
+    #       writeY = list(d[k])[ind][1]
+    ##      statement += str(round(writeY, 3))
+    #      if k == key[-1]:
+    #          f.write(f"{str(round(writeY, 3))}")
+    #       else:
+    #         f.write(f"{str(round(writeY, 3))} ")
+    # print(f"({x[e]},{3})")
     # print("finishito")
 
-
 f.close()
+print("Now complete!")
 
-#program works! <100 lines !!!!!
+# program works! <100 lines !!!!!
